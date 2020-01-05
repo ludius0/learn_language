ac26062e2c59a9bc3f6e5ab300723f960a28f5f4
ludius <57571014+ludius0@users.noreply.github.com>
2020-01-05 13:12:13 +0100

    Changed small things.

--- time program.py
+++ time program.py
@@ -1,61 +1,63 @@
+import time
 from datetime import datetime
 import calendar
 
-curr_time = datetime.now()
 
 class menu():
 
-    def current_time():
+    def displey_current_time():
         now = datetime.now()
-        print(f"{now}" + "\n")
-    
-    def year():
-        formatted_time = curr_time.strftime("%Y")
-        print(f"{formatted_time}" + "\n")
+        print("\n" + f"{now}" + "\n")
 
-    def month():
-        formatted_time = curr_time.strftime("%m")
-        print(f"{formatted_time}" + "\n")
+    def displey_year():
+        print(time.strftime("\n" + "%Y" + "\n"))
 
-    def minute():
-        formatted_time = curr_time.strftime("%M")
-        print(f"{formatted_time}" + "\n")
+    def displey_month():
+        print(time.strftime("\n" + "%m" + "\n"))
 
-    def second():
-        formatted_time = curr_time.strftime("%S")
-        print(f"{formatted_time}" + "\n")
+    def displey_minute():
+        print(time.strftime("\n" + "%M" + "\n"))
 
-    def find_user_day():
+    def displey_second():
+        print(time.strftime("\n" + "%S" + "\n"))
+
+    def displey_user_day():
         import datetime
         try:
             user_date = input("Write date (for example: 04 January 2020): ")
             resulted_day = datetime.datetime.strptime(user_date, "%d %B %Y").weekday()
-            print(f"{calendar.day_name[resulted_day]}" + "\n")
+            print(f"\n{calendar.day_name[resulted_day]}" + "\n")
         except:
             print("Error...\n")
 
-main_menu = True
 
-while main_menu:
+while True:
+    
+    time = datetime.now()
+        
     print("Choose action:\n1. Write a current time\n2. Write a current year\n3. Write a current month\n4. Write a current minute\n5. Write a current seconds\n6. Find a day\n7. End program")
     user_input = input("Write a number: ")
     if user_input == "1" or user_input == " 1":
-        menu.current_time()
+        menu.displey_current_time()
+        
     elif user_input == "2" or user_input == " 2":
-        menu.year()
+        menu.displey_year()
+        
     elif user_input == "3" or user_input == " 3":
-        menu.month()
+        menu.displey_month()
+        
     elif user_input == "4" or user_input == " 4":
-        menu.minute()
+        menu.displey_minute()
+        
     elif user_input == "5" or user_input == " 5":
-        menu.second()
+        menu.displey_second()
+        
     elif user_input == "6" or user_input == " 6":
-        menu.find_user_day()
+        menu.displey_user_day()
+        
     elif user_input == "7" or user_input == " 7":
-        main_menu = False
+        break
     else:
         print("Invalid number")
 
 print("Goodbye...")
-
-
