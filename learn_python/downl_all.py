15b841892bcaea0fec634804c13c7e3080cb7469
ludius0 <playerofzero@icloud.com>
2021-12-16 17:01:00 +0100

    test 4

--- learn_python/downl_all.py
+++ learn_python/downl_all.py
@@ -1,6 +1,6 @@
 import os
 
-a = ['calculator', 'Bitcoin-price-tracker', 'simple_time_program', 'login_system', 'simple_deep_learning', 'Chess-GUI', 'Conway-s-game-of-life', 'simple_hangman_game', 'CartPole_Gym', '2048-game', 'login_data', 'simple_database', 'pixel-painter', 'simulations', 'sorting_algorithm', 'face-recognition', 'Path-finding-algorithm-GUI', 'weather_app', 'automate_git.py', 'sudoku-solver', 'Sorting-visualiser', 'snake']
+a = ['calculator', 'Bitcoin-price-tracker', 'simple_time_program', 'login_system', 'simple_deep_learning', 'Chess-GUI', 'Conway-s-game-of-life', 'simple_hangman_game', 'CartPole_Gym', '2048-game', 'login_data', 'simple_database', 'pixel-painter', 'simulations', 'sorting_algorithm', 'face-recognition', 'Path-finding-algorithm-GUI', 'weather_app', 'automate_git', 'sudoku-solver', 'Sorting-visualiser', 'snake']
 
 for i in a:
     os.system(f'git clone https://github.com/ludius0/{i}.git')
